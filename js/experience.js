65078d4c7da4c9085c8ade6a7d03a73dd3dd23ad
lijun007 <qaz963li@126.com>
2017-06-21 15:52:23 +0800

    修改

--- js/experience.js
+++ js/experience.js
@@ -13,25 +13,4 @@ $(function(){
         },3000)
     })
 
-/*
-    $('#openApp').click(function(e){
-
-        if(navigator.userAgent.match(/(iPhone|iPod|iPad);?/i))
-        {
-            window.location.href = "testYMH";//ios app协议
-            window.setTimeout(function() {
-                window.location.href = "itms-app://itunes.apple.com/cn/app/yu-mei-hui-ke-hu-duan/id1161081835?mt=8";
-            }, 2000)
-        }
-/!*
-        if(navigator.userAgent.match(/android/i))
-        {
-            window.location.href = "com.baidu.tieba://app";//android app协议
-            window.setTimeout(function() {
-                window.location.href = "https://!****.apk";//android 下载地址
-            }, 2000)
-        }
-*!/
-    });
-*/
 })
