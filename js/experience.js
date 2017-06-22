0bb02034e5dc4e7a9c3a35c6a39ac6006fb31e98
lijun007 <qaz963li@126.com>
2017-06-22 14:31:19 +0800

    修改

--- js/experience.js
+++ js/experience.js
@@ -10,55 +10,57 @@ $(function(){
         if(/android/i.test(navigator.userAgent)){
             document.body.appendChild(ifr);
             ifr.src = "xl://ymh:8888/FirstActivity";//Android app协议
-            $(document).on('visibilitychange webkitvisibilitychange', function() {
-                var tag = document.hidden || document.webkitHidden;
-                if (tag) {
-                    clearTimeout(timer);
-                }
-            })
-            $(window).on('pagehide', function() {
-                clearTimeout(timer);
-            })
             timer=setInterval(function(){
                 document.body.removeChild(ifr);
                 window.location.href = "http://itunes.apple.com/cn/app/yu-mei-hui-ke-hu-duan/id1161081835?mt=8";
+                $(document).on('visibilitychange webkitvisibilitychange', function() {
+                    var tag = document.hidden || document.webkitHidden;
+                    if (tag) {
+                        clearTimeout(timer);
+                    }
+                })
+                $(window).on('pagehide', function() {
+                    clearTimeout(timer);
+                })
+
             },1000)
         };
         if(/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)){
             window.location.href = "testYMH://";//ios app协议
-            $(document).on('visibilitychange webkitvisibilitychange', function() {
-                var tag = document.hidden || document.webkitHidden;
-                if (tag) {
-                    clearTimeout(timer);
-                }
-            })
-            $(window).on('pagehide', function() {
-                clearTimeout(timer);
-            })
-
             timer=setInterval(function(){
                 window.location.href = "http://itunes.apple.com/cn/app/yu-mei-hui-ke-hu-duan/id1161081835?mt=8";
+                $(document).on('visibilitychange webkitvisibilitychange', function() {
+                    var tag = document.hidden || document.webkitHidden;
+                    if (tag) {
+                        clearTimeout(timer);
+                    }
+                })
+                $(window).on('pagehide', function() {
+                    clearTimeout(timer);
+                })
             },1000)
 
         }
-/*
-        if(/android/i.test(navigator.userAgent)){
-            window.location.href = "xl://ymh:8888/FirstActivity";//Android app协议
-            window.setTimeout(function(){
-                //
-               // window.location.href = "http://itunes.apple.com/cn/app/yu-mei-hui-ke-hu-duan/id1161081835?mt=8";
-            },3000)
 
-        }
-        if(/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)){
-            window.location.href = "testYMH://";//ios app协议
-            window.setTimeout(function(){
-                // document.body.removeChild(ifr);
-                window.location.href = "http://itunes.apple.com/cn/app/yu-mei-hui-ke-hu-duan/id1161081835?mt=8";
-            },3000)
-        }
-*/
+        /*
+                if(/android/i.test(navigator.userAgent)){
+                    window.location.href = "xl://ymh:8888/FirstActivity";//Android app协议
+                    window.setTimeout(function(){
+                        //
+                       // window.location.href = "http://itunes.apple.com/cn/app/yu-mei-hui-ke-hu-duan/id1161081835?mt=8";
+                    },3000)
+
+                }
+                if(/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)){
+                    window.location.href = "testYMH://";//ios app协议
+                    window.setTimeout(function(){
+                        // document.body.removeChild(ifr);
+                        window.location.href = "http://itunes.apple.com/cn/app/yu-mei-hui-ke-hu-duan/id1161081835?mt=8";
+                    },3000)
+                }
+        */
     })
+
 })
 /*
 var openUrl='testYMH://'
@@ -123,3 +125,4 @@ var BrowserInfo = function() {
 
     return json;
 };*/
+
