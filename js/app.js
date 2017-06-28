f4768da20dcab8685e25f0447e4dc1945ef52a4d
lijun007 <qaz963li@126.com>
2017-06-28 14:11:36 +0800

    修改

--- js/app.js
+++ js/app.js
@@ -7,13 +7,6 @@
     var rem=viewW/6.4;
     html.style.fontSize=rem+'px'
 })();
-$(function(){
-    $(window).scroll(function(){
-        var propH=$('html').height()/*+$(window).scrollTop()*/;
-        $('.prop').css('height',propH);
-        $('.guidePage').css('height',propH);
-    });
-});
 var app=angular.module('app',['ui.router']);
 app.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider,$urlRouterProvider){
     //默认路由到mainTab
@@ -37,33 +30,7 @@ app.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider,$ur
                 }
             }
         })
-}])
-app.factory('open',function(){
-    return {
-       openApp:function(){
-           var timer;
-           if(/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)){
-               // console.log(111)
-               if(/MicroMessenger/ig.test(navigator.userAgent)||/qq/ig.test(navigator.userAgent)){
-                   $('.guidePage').show();
-               }else {
-                   $('.guidePage').hide();
-                   window.location.href = "testYMH://";//ios app协议
-                   timer=setInterval(function(){
-                       window.location.href = "itms-apps://itunes.apple.com/cn/app/yu-mei-hui-ke-hu-duan/id1161081835?mt=8";
-                   },1000)
-
-               }
-           };
-           if(/android/i.test(navigator.userAgent)){
-               window.location.href= "xl://ymh:8888/FirstActivity";//Android app协议
-               timer=setInterval(function(){
-                   window.location.href = "itms-apps://itunes.apple.com/cn/app/yu-mei-hui-ke-hu-duan/id1161081835?mt=8";
-               },1000)
-           };
-       }
-    }
-});
+}]);
 app.factory('scroll',function(){
     return {
         height:function(){
@@ -71,5 +38,35 @@ app.factory('scroll',function(){
             $('.prop').css('height',propH);
             $('.guidePage').css('height',propH);
         }
+    }
+});
+app.factory('open',function(){
+    return {
+       openApp:function(){
+           var timer;
+           if(/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)){
+               // console.log(111)
+
+               if(/MicroMessenger/ig.test(navigator.userAgent)||/qq/ig.test(navigator.userAgent)){
+                   $('.guidePage').show();
+               }else {
+                   $('.guidePage').hide();
+
+                   window.location.href = "testYMH://";//ios app协议
+                   timer=setInterval(function(){
+                       window.location.href = "itms-apps://itunes.apple.com/cn/app/yu-mei-hui-ke-hu-duan/id1161081835?mt=8";
+                   },3000)
+               }
+           };
+           if(/android/i.test(navigator.userAgent)){
+               window.location.href= "xl://ymh:8888/FirstActivity";//Android app协议
+               timer=setInterval(function(){
+                   window.location.href = "itms-apps://itunes.apple.com/cn/app/yu-mei-hui-ke-hu-duan/id1161081835?mt=8";
+               },3000)
+           };
+           $(window).onload(function(){
+              clearInterval(timer)
+           })
+       }
     }
 });
